dfe3f96f66bcf3e189e52545161ecfeca1ebbf72
Darren <darren.daley@hotmail.co.uk>
2020-01-25 13:59:06 +0000

    recursion implemented

---
+++ Recursion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountDown(10);
+        }
+
+        private static void CountDown(int i)
+        {
+            if(i <= 0) {
+                return;
+            }
+
+            Console.WriteLine(i);
+            CountDown(i - 1);
+        }
+    }
+}
