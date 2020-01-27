5a251e0ef6340af1bbd5ba82eb892d717d498c4a
Darren <darren.daley@hotmail.co.uk>
2020-01-27 16:22:25 +0000

    Implemented Breadth First, started implementation on Dijkstras

---
+++ Breadth First Search/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Breadth_First_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personDict = new Dictionary<string, List<string>>();
+
+            personDict.Add("Darren", new List<string>(new string[] { "Alice", "Bob", "Claire" }));
+            personDict.Add("Bob", new List<string>(new string[] { "Anuj", "Peggy" }));
+            personDict.Add("Alice", new List<string>(new string[] { "Peggy" }));
+            personDict.Add("Claire", new List<string>(new string[] { "Thom", "Johnny" }));
+            personDict.Add("Anuj", new List<string>(new string[] { }));
+            personDict.Add("Peggy", new List<string>(new string[] { }));
+            personDict.Add("Thom", new List<string>(new string[] { }));
+            personDict.Add("Johnny", new List<string>(new string[] { }));
+
+            Search("Darren", personDict);
+        }
+
+        private static void Search(string personName, Dictionary<string, List<string>> personDict)
+        {
+            var searchQueue = new Queue<string>(personDict[personName]);
+            var searched = new List<string>();
+
+            while (searchQueue.Count > 0)
+            {
+                var person = searchQueue.Dequeue();
+
+                if(!searched.Exists(p => p == person))
+                {
+                    if(IsPersonSeller(person)) 
+                    {
+                        Console.WriteLine(person + " is a mango seller !");
+                        return;
+                    }
+
+                    personDict[person].ForEach(p => searchQueue.Enqueue(p));
+                    searched.Add(person);
+                }
+            }
+
+            Console.WriteLine("There is no mango seller !");
+        }
+
+        private static bool IsPersonSeller(string name)
+        {
+            if(char.ToLowerInvariant(name.ToCharArray()[name.Length - 1]) == 'm') {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Dijkstras Algorithm/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dijkstras_Algorithm
+{
+    public struct Node
+    {
+        public string Key { get; }
+        public string Name { get; }
+        public int Weight { get; }
+
+        public Node(string key, string name, int weight)
+        {
+            this.Key = key;
+            this.Name = name;
+            this.Weight = weight;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Node> graph = new List<Node>();
+            graph.Add(new Node("Start", "A", 6));
+            graph.Add(new Node("Start", "B", 2));
+            graph.Add(new Node("A", "Fin", 1));
+            graph.Add(new Node("B", "A", 3));
+            graph.Add(new Node("B", "Fin", 5));
+            graph.Add(new Node("Fin", string.Empty, 1));
+
+            Dictionary<string, int> costs = new Dictionary<string, int>();
+            costs.Add("A", 6);
+            costs.Add("B", 2);
+            costs.Add("Fin", int.MaxValue);
+
+
+            Dictionary<string, string> parents = new Dictionary<string, string>();
+            parents.Add("A", "Start");
+            parents.Add("B", "Start");
+            parents.Add("Fin", string.Empty);
+
+            Dijkstras(graph, costs, parents);
+
+            foreach (var item in costs) {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+
+            Console.WriteLine('\n');
+
+            foreach (var item in parents) {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+        }
+
+        private static void Dijkstras(List<Node> graph, Dictionary<string, int> costs, Dictionary<string, string> parents)
+        {
+            var processed = new List<Node>();
+            var node = LowestCodeNode(costs, graph, processed);
+
+            while (node.Key != null)
+            {
+                var cost = costs[node.Name];
+                var neighbours = new List<Node>();
+                neighbours.AddRange(graph.FindAll(node => node.Key == node.Name));
+
+                for (int i = 0; i < neighbours.Count; i++)
+                {
+                    if(string.IsNullOrEmpty(neighbours[i].Name)) {
+                        continue;
+                    }
+
+                    var newCost = cost + neighbours[i].Weight;
+                    var value = costs[neighbours[i].Name];
+
+                    if(value > newCost)
+                    {
+                        costs[neighbours[i].Name] = newCost;
+                        parents[neighbours[i].Name] = node.Name;
+                    }
+                }
+
+                processed.Add(node);
+                node = LowestCodeNode(costs, graph, processed);
+            }
+        }
+
+        private static Node LowestCodeNode(Dictionary<string, int> costs, List<Node> graph, List<Node> processed)
+        {
+            var lowestCost = int.MaxValue;
+            var lowestCostNode = new Node();
+
+            foreach (var node in costs)
+            {
+                var cost = node.Value;
+
+                if(cost < lowestCost && !processed.Exists(n => n.Key == node.Key))
+                {
+                    lowestCost = cost;
+                    lowestCostNode = graph.Find(n => n.Name == node.Key);
+                }
+            }
+
+            return lowestCostNode;
+        }
+    }
+}
